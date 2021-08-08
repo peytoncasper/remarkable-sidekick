648a40b87b03f93a79f41d87be26a9acd8ac0be9
Peyton Casper <peytoncas@gmail.com>
2021-08-08 15:59:24 -0400

    Adding additional ACL Policies for GCS and Marketing Site Github Action

--- terraform/marketing-page/main.tf
+++ terraform/marketing-page/main.tf
@@ -1,9 +1,13 @@
+data "google_service_account" "github_actions" {
+  account_id = "github-action"
+}
+
 resource "google_storage_bucket" "marketing_site" {
   name          = "remarkablesidekick.com"
   location      = "US"
   force_destroy = true
 
-  uniform_bucket_level_access = true
+  uniform_bucket_level_access = false
 
   website {
     main_page_suffix = "index.html"
@@ -21,4 +25,10 @@ resource "google_storage_bucket_access_control" "public_rule" {
   bucket = google_storage_bucket.marketing_site.name
   role   = "READER"
   entity = "allUsers"
+}
+
+resource "google_storage_bucket_access_control" "github_action_uploader" {
+  bucket = google_storage_bucket.marketing_site.name
+  role   = "WRITER"
+  entity = "user-${data.google_service_account.github_actions.email}"
 }
